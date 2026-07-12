000a7e1677b0095bfcadab9c22bebb9788b9e364
Shahida Batool <55743409+shahidabatool@users.noreply.github.com>
2026-07-12 17:53:23 -0400

    Refactor database initialization script
    Removed existing table creation and drop statements, and added database and schema creation logic.

--- script/Bronze/init_database.sql
+++ script/Bronze/init_database.sql
@@ -1,70 +1,41 @@
--- if object already exist then create 
-if OBJECT_ID('bronze.crm_cust_info','U') is NOT NULL 
-    drop table bronze.crm_cust_info
-create  table bronze.crm_cust_info(
-    cst_id int,
-    cst_key NVARCHAR(50),
-    cst_first_name NVARCHAR(50),
-    cst_last_name NVARCHAR(50),
-    cst_marital_status NVARCHAR(50),
-    cst_gndr NVARCHAR(50),
-    cst_created_date DATETIME,
-);
+use master;
+GO
 
+IF DB_ID('DataWarehouse') IS NULL
+BEGIN
+    CREATE DATABASE DataWarehouse;
+END;
+GO
 
-if OBJECT_ID('bronze.crm_prd_info','U') is NOT NULL
-    drop table bronze.crm_prd_info
+use DataWarehouse;
+GO
+-- create Schema
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.schemas
+    WHERE name = 'bronze'
+)
+BEGIN
+    EXEC('CREATE SCHEMA bronze');
+END;
+GO
 
-create table bronze.crm_prd_info(
-    prd_id int,
-    prd_key NVARCHAR(50),
-    prd_nm NVARCHAR(50),
-    prd_cost int,
-    prd_line NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt DATETIME
-);
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.schemas
+    WHERE name = 'silver'
+)
+BEGIN
+    EXEC('CREATE SCHEMA silver');
+END;
+GO
 
-
-if OBJECT_ID('bronze.crm_sales_details','U') is NOT NULL
-    drop table bronze.crm_sales_details
-create table bronze.crm_sales_details(
-    sls_ord_num NVARCHAR(50),
-    sls_prd_key NVARCHAR(50),
-    sls_cust_id int,
-    sls_order_dt INT,
-    sls_ship_dt INT,
-    sls_due_dt INT,
-    sls_sales  int,
-    sls_quantity int,
-    sls_price int
-);
-
-
-
-if OBJECT_ID('bronze.erp_loc_a101','U') is NOT NULL
-    drop table bronze.erp_loc_a101
-create table bronze.erp_loc_a101(
-    cid NVARCHAR(50),
-    cntry NVARCHAR(50),
-);
-
-
-
-if OBJECT_ID('bronze.erp_cust_az12','U') is NOT NULL
-    drop table bronze.erp_cust_az12
-create table bronze.erp_cust_az12(
-    cid NVARCHAR(50),
-    bdate DATE,
-    gen NVARCHAR(50)
-);
-
-
-if OBJECT_ID('bronze.erp_px_cat_g1v2','U') is NOT NULL
-    drop table bronze.erp_px_cat_g1v2
-create table bronze.erp_px_cat_g1v2(
-    id NVARCHAR(50),
-    cat NVARCHAR(50),
-    subcat NVARCHAR(50),
-    maintenance NVARCHAR(50)
-);
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.schemas
+    WHERE name = 'gold'
+)
+BEGIN
+    EXEC('CREATE SCHEMA gold');
+END;
+GO
